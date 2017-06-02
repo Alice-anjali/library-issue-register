5cd4d529880d64c105d7a065e937e5925864bfc7
Alice-anjali <alice.anjali1995@gmail.com>
2017-06-03 00:59:22 +0530

    login route completed

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var Admin = require('../models/admin');
+var app = express();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,7 +10,14 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/login', function(req, res, next) {
-  res.sendFile(path.resolve(__dirname + '/../public/dashboard.html'));
+  Admin.findOne({admin : req.body.username, password : req.body.password},function(err,admin){
+    if(admin){
+      res.sendFile(path.resolve(__dirname + '/../public/dashboard.html'));
+    }
+    else{
+      res.redirect('/');
+    }
+  })
 });
 
 module.exports = router;
