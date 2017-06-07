9ded391c3a3a4bbe26ec7b77429f81da93dfbd1a
Alice-anjali <alice.anjali1995@gmail.com>
2017-06-07 20:31:51 +0530

    buttons added

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var Admin = require('../models/admin');
+var issueList = require('../models/issuelist');
 var app = express();
 
 /* GET home page. */
@@ -31,7 +32,38 @@ router.get('/logout', function(req,res,next){
   res.redirect('/');
 });
 
+// router.get('/dashboard', loggedincheck, function(req,res,next){
+//   issueList.find({},function(err, getIssues){
+//     if(err){
+//       return handleError(err);
+//     }
+//     else{
+//       res.render('dashboard', {IssueList : getIssues});
+//     }
+//   });
+// });
 
+router.get('/dashboard', function(req,res,next){
+    res.render('dashboard', {});
+});
 
+// var loggedincheck = function(req,res,next){
+//   console.log("loggedincheck......");
+//   return next();
+// }
+
+// var loggedincheck = function(req,res,next){
+//   if(req.session){
+//     if(req.session.admin == 'library@cet'){
+//       next();
+//     }
+//     else{
+//       res.redirect('/');
+//     }
+//   }
+//   else{
+//     res.redirect('/');
+//   }
+// }
 
 module.exports = router;
